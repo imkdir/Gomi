59787f2e9188296e335b38f7d981576db68b457e
Tung CHENG <hichengd@gmail.com>
2019-06-05 23:28:23 +0800

    Initial Commit

---
+++ Gomi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gomi
+//
+//  Created by Tung CHENG on 2019/6/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
